0962ed752b62b753726abff3b35a09186c8e1045
shiprasingh08 <shiprasinghmau8@gmail.com>
2026-02-05 22:43:44 +0530

    ..

---
+++ Chapter 4/01_list.py
@@ -0,0 +1,4 @@
+friends = ["Apple", "Orange",5, 345.06, False, "Akash","Rohan"]
+print(friends[0])
+friends[0] = "Banana" # unlike string list is mutable
+print(friends[0])
---
+++ Chapter 4/02_list_methods.py
@@ -0,0 +1,13 @@
+friends = ["Apple", "Orange",5, 345.06, False, "Akash","Rohan"]
+print(friends)
+friends.append("Aditya") # append means add element at the end of the list
+
+print(friends)
+
+
+l1 = [1, 34, 64,2,6,11]
+#l1.sort() # sort the list in ascending order
+#l1.reverse() # reverse the list in descending order
+l1.insert(3, 33333) # insert 33333 such that its index in the list is 3
+l1
+print(l1)
